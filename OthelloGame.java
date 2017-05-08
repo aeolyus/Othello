656e1f55f6bc0d5b952d7e651b0198c8dc38a3c1
Richard Huang <richardhuang.huang@gmail.com>
2017-05-08 15:03:54 +0000

    theoretical capture, fixed printboard, incomplete legal

--- OthelloGame.java
+++ OthelloGame.java
@@ -10,14 +10,78 @@ public class Othello implements Game{
         play();
     }
     public boolean isLegal(int i, int j, int player){
-        //TODO: implement later
-        for (int y = 0; y < i; y++){
-            
+        boolean canDo = false;
+        int otherPlayer;
+        if (player == 1){
+            otherPlayer == 2;
+        }else if (player == 2){
+            otherPlayer == 1;
         }
+        
+        if (board[i][j] == player || board[i][j] == otherPlayer){
+            System.out.print("Error: That space is not empty");
+            return canDo;
+        }
+        
+        int topLeft = board[i-1][j-1];
+        if (topLeft == otherPlayer){
+            int y = j-2;
+            for (int x = i-2; x >= 0; x--){
+                if (board[x][y] == player){
+                    canDo = true;
+                    break;
+                }else if(board[x][y] == 0){
+                    break;
+                }else(board[x][y] == otherPlayer){
+                    y--;
+                }
+            }
+        }
+        int topMid = board[i-1][j];
+        if (topMid == otherPlayer){
+            for (int x = i-2; x >= 0; x--){
+                if (board[x][y] == player){
+                    canDo = true;
+                    break;
+                }else if(board[x][y] == 0){
+                    break;
+                }else(board[x][y] == otherPlayer){
+                    y--;
+                }
+            }
+        }
+        int topRight = board[i-1][j+1];
+        int midLeft = board[i][j-1];
+        int midRight = board[i][j+1];
+        int botLeft = board[i+1][j-1];
+        int botMid = board[i+1][j];
+        int botRight = board[i+1][j+1];
     }
     public void play(){
+        Scanner in = new Scanner(System.in);
         while(!gameOver()){
-            //TODO: current player plays
+            System.out.println("Player 1, Enter x-coord");
+            int x = in.nextInt();
+            System.out.println("Player 1, Enter y-coord");
+            int y = in.nextInt();
+            while (isLegal(x, y, 1) != true){
+                System.out.println("Error: Invalid move")
+                System.out.println("Player 1, Enter x-coord");
+                int x = in.nextInt();
+                System.out.println("Player 1, Enter y-coord");
+                int y = in.nextInt();
+            }
+            System.out.println("Player 2, Enter x-coord");
+            int x = in.nextInt();
+            System.out.println("Player 2, Enter y-coord");
+            int y = in.nextInt();
+            while (isLegal(x, y, 1) != true){
+                System.out.println("Error: Invalid move")
+                System.out.println("Player 2, Enter x-coord");
+                int x = in.nextInt();
+                System.out.println("Player 2, Enter y-coord");
+                int y = in.nextInt();
+            }
             board.printBoard();
         }
     }
@@ -28,7 +92,7 @@ public class Othello implements Game{
         if(board.full()){
             for(int i=0;i<8;i++)
                 for(int j=0;j<8;j++)
-                    if(isLegal(i,j,currentPlayer)
+                    if(isLegal(i,j,currentPlayer))
                         return false;
             return true;
             int winner=board.getScore1()>board.getScore2()?1:2;
