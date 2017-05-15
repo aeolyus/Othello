320d14c4c74778469c58c7c7b7aa016582058d0a
Richard Huang <richardhuang.huang@gmail.com>
2017-05-14 22:44:59 -0700

    added really bad PvCPU

--- src/OthelloGame.java
+++ src/OthelloGame.java
@@ -2,6 +2,8 @@ import java.util.*;
 public class OthelloGame implements Game{
     OthelloBoard board;
     private int currentPlayer;
+    private boolean cpu=true;
+    private Scanner in;
     /**
      * Creates a new Othello  game
      */
@@ -13,7 +15,10 @@ public class OthelloGame implements Game{
      * Starts the game
      */
     public void start(){
+    	in=new Scanner(System.in);
         board=new OthelloBoard();
+        System.out.print("Enter 1 for PvP or 2 for PvCPU: ");
+        cpu=in.nextInt()>1?true:false;
         play(1);
     }
     
@@ -24,24 +29,54 @@ public class OthelloGame implements Game{
      * @param player current player
      */
     public void play(int player){
-    	Scanner in=new Scanner(System.in);
     	int x,y;
     	board.printBoard();
     	if(!gameOver()){
-    		System.out.print("Player "+player+" , Enter y-coord: ");
-    		x=in.nextInt();
-    		System.out.print("Player "+player+" , Enter x-coord: ");
-    		y=in.nextInt();
-    		System.out.println();
-    		if(board.isLegal(x,y,player)){
-    			board.place(x,y,player);
-    			if(player==1)
-    				play(2);
-    			else
-    				play(1);
+    		if(!cpu){
+    			System.out.print("Player "+player+" , Enter y-coord: ");
+    			x=in.nextInt();
+    			System.out.print("Player "+player+" , Enter x-coord: ");
+    			y=in.nextInt();
+    			System.out.println();
+    			if(board.isLegal(x,y,player)){
+    				board.place(x,y,player);
+    				if(player==1)
+    					play(2);
+    				else
+    					play(1);
+    			}else{
+    				System.out.println("Illegal move. Try again.");
+    				play(player);
+    			}
     		}else{
-    			System.out.println("Illegal move. Try again.");
-    			play(player);
+    			if(player==1){
+    				System.out.print("Player "+player+" , Enter y-coord: ");
+        			x=in.nextInt();
+        			System.out.print("Player "+player+" , Enter x-coord: ");
+        			y=in.nextInt();
+        			System.out.println();
+        			if(board.isLegal(x,y,player)){
+        				board.place(x,y,player);
+        				if(player==1)
+        					play(2);
+        				else
+        					play(1);
+        			}else{
+        				System.out.println("Illegal move. Try again.");
+        				play(player);
+        			}
+    			}else if(player==2){//cpu
+    				boolean placed=false;
+    				for(int i=0;i<8&&!placed;i++){
+    					for(int j=0;j<8&&!placed;j++){
+    						if(board.isLegal(i,j,player)){
+    							placed=true;
+    							board.place(i,j,player);
+    						}
+    					}
+    				}
+    				play(1);
+    			}
     		}
     	}else{
     		int winner=board.getScore1()>board.getScore2()?1:2;
